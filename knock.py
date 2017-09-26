114085d20b79e9db2661f276ff96e72b9a117513
TimeFur <djs860403@gmail.com.tw>
2017-09-26 23:12:05 +0800

    Add the mouse listener
    1. Declare the on_move, on_click & on_scroll as callback function
    2. Using the mosue event listener to listen event
    
    ref: http://pythonhosted.org/pynput/mouse.html

--- knock.py
+++ knock.py
@@ -1,11 +1,31 @@
 import os
+from pynput.mouse import Listener
+
+def on_move(x, y):
+    print ("Pointer move to {0}".format(
+            (x, y)))
+
+def on_click(x, y, button, pressed):
+    print ('{0} at {1}'.format(
+            'Pressed' if pressed else 'Release',
+            (x, y)))
+    
+def on_scroll(x, y, dx, dy):
+    print ('Scrolled {0}'.format(
+            (x, y)))
+    return False
 
 def openfolder(path):
     os.startfile(path)
     
 def main():
     print "Knock center"
-    openfolder("D:")
+    #openfolder("D:")
+    with Listener(
+            on_move = on_move,
+            on_click = on_click,
+            on_scroll = on_scroll) as listener:
+        listener.join()
     
 if __name__ == "__main__":
     main()
